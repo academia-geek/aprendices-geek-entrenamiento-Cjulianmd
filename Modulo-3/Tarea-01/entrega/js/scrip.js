0559cc074dbc5527e7fd185e63035a8e47237ef6
cjulianmd <julian.gallo.v@gmail.com>
2022-06-09 20:09:08 -0500

    tarea-1,resuelta

--- Modulo-3/Tarea-01/entrega/js/scrip.js
+++ Modulo-3/Tarea-01/entrega/js/scrip.js
@@ -1,31 +1,192 @@
-
-(function(){
-    const sliders = [...document.querySelectorAll(".personaje")];
-    const arrowSiguiente = document.querySelectorAll("#siguiente");
-    const arrowAntes = document.querySelector('#antes')
+    
+    
+    
+    (function(){
+    const sliders = [...document.querySelectorAll(".slider_body")];
+    const arrowSiguiente = document.querySelector("#next");
+    const arrowAntes = document.querySelector('#before');
     let value;
 
     arrowSiguiente.addEventListener("click", () =>changePosition(1));
     arrowAntes.addEventListener("click", () =>changePosition(-1));
 
     function changePosition(change){
-        const currentElement = Number(document.querySelector('.personaje1')
-        .dataset.id);
+        const currentElement = Number(document.querySelector('.slider_body--shadow').
+        dataset.id);
+        
         value = currentElement;
         value+= change;
-        sliders[value-1].classList.toggle(".personaje1");
+
+        console.log(sliders.length);
+        if(value === 0 || value == sliders.length+1){
+            value = value === 0 ? sliders.length : none;
+
+
+        }
+        sliders[currentElement-1].classList.toggle("slider_body--shadow");
+        sliders[value-1].classList.toggle("slider_body--shadow");
+
     }
 })()
+    function Golem1() {
+            document.getElementById("G").style.display="block";
+        }
+    function cerrar1() {
+            document.getElementById("G").style.display="none";
+    }
+    function Golem2() {
+        document.getElementById("A").style.display="block";
+    }
+    function cerrar2() {
+            document.getElementById("A").style.display="none";
+    }
+    function Golem3() {
+        document.getElementById("b").style.display="block";
+    }
+    function cerrar3() {
+            document.getElementById("b").style.display="none";
+    }
+    function Golem4() {
+        document.getElementById("c").style.display="block";
+    }
+    function cerrar4() {
+            document.getElementById("c").style.display="none";
+    }
+    function Golem5() {
+        document.getElementById("d").style.display="block";
+    }
+    function cerrar5() {
+            document.getElementById("d").style.display="none";
+    }
+    function Golem6() {
+        document.getElementById("g").style.display="block";
+    }
+    function cerrar6() {
+            document.getElementById("g").style.display="none";
+    }
+    function Golem7() {
+        document.getElementById("l").style.display="block";
+    }
+    function cerrar7() {
+            document.getElementById("l").style.display="none";
+    }
+    function Golem8() {
+        document.getElementById("M").style.display="block";
+    }
+    function cerrar8() {
+            document.getElementById("M").style.display="none";
+    }
+    function Golem9() {
+        document.getElementById("m").style.display="block";
+    }
+    function cerrar9() {
+            document.getElementById("m").style.display="none";
+    }
+    function Golem10() {
+        document.getElementById("P").style.display="block";
+    }
+    function cerrar10() {
+            document.getElementById("P").style.display="none";
+    }
+    function Golem11() {
+        document.getElementById("p").style.display="block";
+    }
+    function cerrar11() {
+            document.getElementById("p").style.display="none";
+    }
+
+
+    
+     /*
+        
     
     
     
     
     
+    const buttonBefore = document.getElementById("before")
+    const buttonAfter = document.getElementById("after")
+    
+    const pjImg = document.getElementById("pj-image")
+    const pjButton = document.getElementById("pj-button")
+    
+    const pjImgInfo = document.getElementById("pj-image-info")
+    
+    const pjNameInfo = document.getElementById("info-name")
+    //const pjTextInfo = document.getElementById("info-text")
+    const pjTextInfo = document.querySelector("#info-text")
+    
+    let pjNum = 0
+    
+    let pjName 
+    
+    let img = [
+        "golem.png",
+        "Pekka.webp",
+        "caballero.png",
+        "bruja.png",
+        "los-pillos.png",
+        "goblin.webp",
+        "arquero.png",
+        "montaPuercos.webp",
+        "Miner_info.webp",
+        "principe.png",
+    ]
+    
+    buttonAfter.addEventListener("click",function(event){
+        if(pjNum > 0) {
+            pjNum--
+            //console.log(pjName[pjNum])
+            createCard(pjName[pjNum])
+            buttonBefore.className = "button-able"
+        }
+        if(pjNum == 0){
+            buttonAfter.className = "button-disable"
+        }
+    })
+    
+    buttonBefore.addEventListener("click",function(event){
+        if(pjNum < pjName.length-1) {
+            pjNum++
+            //console.log(pjName[pjNum])
+            createCard(pjName[pjNum])
+            buttonAfter.className = "button-able"
+        }
+        if(pjNum == pjName.length-1){
+            buttonBefore.className = "button-disable"
+        }
+    })
+    
+    function createCard(name){
+        pjImg.setAttribute("src","./img/"+imagen[pjNum])
+        pjImgInfo.setAttribute("src","./img/"+imagen[pjNum])
+    
+        pjButton.innerText = name
+        let {pjName, text} = getInLocalStorage(name)
+    
+        pjNameInfo.innerText = pjName
+        pjTextInfo.innerText = text
+    
+    }
+    
+    function getInLocalStorage(name){
+        let pjText = localStorage.getItem(name)
+        return {
+            pjName : name,
+            text : pjText
+        }
+    }
+    */
     
     
     
     
-    /*let pocision = 0;
+    /*
+
+
+
+
+    let pocision = 0;
     document.addEventListener('DOMContentLoaded', function() {
             let button = document.createElement('button');
             button.type = 'button';
