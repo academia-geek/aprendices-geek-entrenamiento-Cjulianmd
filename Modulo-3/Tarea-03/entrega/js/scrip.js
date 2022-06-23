1ba8f7c87250dda01a607478e9d7c90cd9a416a7
cjulianmd <julian.gallo.v@gmail.com>
2022-06-22 23:38:04 -0500

    tarea-3

---
+++ Modulo-3/Tarea-03/entrega/js/scrip.js
@@ -0,0 +1,71 @@
+import { getData } from "../helpers/getData.js";
+import { compra, odjetos } from "../modules/odjetos.js";
+const inicio = document.getElementById("container")
+const caja1 = document.getElementById("caja1")
+const cont1 = document.querySelector(".container1")
+const bt1 = document.getElementById("b1");
+const bt2 = document.getElementById("b2");
+const bt3 = document.getElementById("b3");
+const bt4 = document.getElementById("b4");
+const url = "http://localhost:4005/"
+
+
+bt1.addEventListener("click", async() =>{
+    try {
+        const data =  await getData(url + "electronics");
+        odjetos(data, caja1 ); 
+        console.log();
+        inicio.style.display = 'none'
+    } catch (error) {
+        
+    }
+
+})
+bt2.addEventListener("click", async() =>{
+    try {
+        const data =  await getData(url + "art");
+        odjetos(data, caja1 ); 
+        console.log();
+        inicio.style.display = 'none'
+    } catch (error) {
+        
+    }
+
+})
+bt3.addEventListener("click", async() =>{
+    try {
+        const data =  await getData(url + "gardens");
+        odjetos(data, caja1 ); 
+        console.log();
+        inicio.style.display = 'none'
+    } catch (error) {
+        
+    }
+
+})
+bt4.addEventListener("click", async() =>{
+    try {
+        const data =  await getData(url + "clothing");
+        odjetos(data, caja1 ); 
+        console.log();
+        inicio.style.display = 'none'
+    } catch (error) {
+        
+    }
+
+})
+bt4.addEventListener("click", async() =>{
+    inicio.style.display = 'block'
+})
+
+let com;
+
+document.addEventListener("click", ({target})=> {
+        console.log(target.classList.contains("imgcos"));
+        if (target.classList.contains("imgcos")){
+            //const info = com.find(p => p.id == target.id)
+            
+                
+            
+        }
+ } )
---
+++ Modulo-3/Tarea-03/entrega/modules/odjetos.js
@@ -0,0 +1,18 @@
+export const odjetos = (data , container) => {
+    container.innerHTML = ""
+    data.forEach((element) => {
+        const {nombre,imgproduto,costo,id} = element
+        container.innerHTML += '<a  href="#" <div id="cajas" class="cajas"><img id='+id+' src='+imgproduto+' class="imgcos buton" alt="hola"><div class="cajita"><h2 class="h1">'+costo+'</h2><h1 class="h2">'+nombre+'</h1></div>></a>'
+    })
+
+}
+export const compra = (data, container) => {
+    container.innerHTML = ""
+    data.forEach((element) => {
+        const {nombre,imgproduto,costo,id} = element
+        container.innerHTML += '<a  href="#" <div id="cajas" class="cajas"><img id='+id+' src='+imgproduto+' class="imgcos buton" alt="hola"><div class="cajita"><h2 class="h1">'+costo+'</h2><h1 class="h2">'+nombre+'</h1></div>></a>'
+    })
+}
+//./pages/index.html
+
+
